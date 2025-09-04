619d1e78627e6ab6ee6b59cc142a162d3b90c367
Mutiara Dina Pasha <mutiaradinapasha@gmail.com>
2025-09-04 13:55:28 +0700

    commit1

---
+++ test_bookmanager.py
@@ -0,0 +1,41 @@
+import unittest
+from book import Book
+from book_manager import BookManager
+
+class TestBookManager(unittest.TestCase):
+    def setUp(self):
+        self.book_manager = BookManager()
+
+    def test_add_book(self):
+        """Test menambahkan buku"""
+        book = Book("Pemrograman", "Andi", 2020)
+        self.book_manager.add_book(book)
+        self.assertEqual(1, self.book_manager.get_book_count())
+
+    def test_remove_existing_book(self):
+        """Test menghapus buku yang ada"""
+        book = Book("Basis Data", "Erlangga", 2021)
+        self.book_manager.add_book(book)
+
+        removed = self.book_manager.remove_book("Basis Data")
+        self.assertTrue(removed)
+        self.assertEqual(0, self.book_manager.get_book_count())
+
+    # Lengkapi Unit Test di bawah untuk buku yang memang tidak terdapat pada list
+    def test_remove_non_existing_book(self):
+        """Test menghapus buku yang tidak ada"""
+        pass
+
+    # Lengkapi Unit Test di bawah untuk mencari buku berdasarkan penulis
+    def test_find_books_by_author(self):
+        """Test mencari buku berdasarkan author"""
+        pass
+
+    # Lengkapi Unit Test di bawah untuk seluruh buku yang ada di dalam list
+    def test_get_all_books(self):
+        """Test mendapatkan semua buku"""
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
